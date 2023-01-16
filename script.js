1f6961bd2a53aaa247cabb4d1926612cfcc755a7
Oumar <2195156@cegepoutaouais.qc.ca>
2023-01-16 12:15:13 -0500

    Ajout description et prix dans ma liste

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Bonjour");
+function ajouter(){
+   let item=document.getElementById("desc").value;
+   let lePrixItem=document.getElementById("valeur").value;
+   let nouveauP=document.createElement("li");
+   let nouveauSpan=document.createElement("span");
+   nouveauP.innerText=item + " : "; //ecrire dans le paragraphe
+
+   nouveauSpan.innerText=lePrixItem + "$";
+document.getElementById("listeItems").append(nouveauP);
+//document.getElementById("listeItems").append(nouveauSpan);
+nouveauP.append(nouveauSpan);
+
+
+
+
+// mettre a jour le total
+   let ancienPrix= document.getElementById("monTotal").innerText;
+   let nouveauPrix = +ancienPrix + +lePrixItem;
+
+   document.getElementById("monTotal").innerText=nouveauPrix;
+
+
+
+    //alert("L'item est deja ajouter");
+}
+
