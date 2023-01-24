0c20efd2fec2c0bdfe2c69690165955c1174e174
Oumar <2195156@cegepoutaouais.qc.ca>
2023-01-24 10:21:40 -0500

    Ajout description et prix dans ma liste et d'autres input

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+compteur=4;
 function ajouter(){
    let item=document.getElementById("desc").value;
    let lePrixItem=document.getElementById("valeur").value;
@@ -10,17 +11,33 @@ document.getElementById("listeItems").append(nouveauP);
 //document.getElementById("listeItems").append(nouveauSpan);
 nouveauP.append(nouveauSpan);
 
-
-
-
 // mettre a jour le total
    let ancienPrix= document.getElementById("monTotal").innerText;
    let nouveauPrix = +ancienPrix + +lePrixItem;
 
    document.getElementById("monTotal").innerText=nouveauPrix;
 
+   //creer des nouveaux items hide dans le formulaire pour envoyer par courriel
+
+   nouveauInput=document.createElement("input");
+   nouveauInput.setAttribute("hidden",true);
+   nouveauInput.setAttribute("name","item"+compteur);
+   nouveauInput.setAttribute("value", item);
+   document.forms[0].append(nouveauInput);
+   compteur++;
+
+
 
 
     //alert("L'item est deja ajouter");
+}
+
+function ajouterEmail(){
+   let email=document.getElementById("email").value;
+   document.forms[0].action="mailto:"+email;
+}
+
+function effacer(){
+   document.forms[0].action="";
 }
 
